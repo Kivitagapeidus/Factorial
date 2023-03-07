ef4ea03fa585b30399b6d984b42704bbb9fc94d2
Kivitagapeidus <s88rumail@gmail.com>
2023-03-07 16:03:35 +0200

    Initial commit

---
+++ test/FactorialTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FactorialTest {
+
+    private final Factorial factorial = new Factorial();
+
+    @Test
+     void calculate_0() {
+       assertEquals(1,factorial.calculate(0));
+    }
+    @Test
+     void calculate_1() {
+       assertEquals(1,factorial.calculate(0));
+    }
+}
